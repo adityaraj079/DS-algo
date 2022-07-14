d31e47abf677d36fcfdf029575bf3550b967fae3
Aditya Raj <rajaditya079@gmail.com>
2022-07-14 22:58:34 +0530

    Binary tree basics done

--- DSA-Bootcamp/Tree/BinaryTree.cpp
+++ DSA-Bootcamp/Tree/BinaryTree.cpp
@@ -7,61 +7,119 @@ class node {
         node* left;
         node* right;
 
-    node(int d) {
+    node(int d)
+    {
         this -> data = d;
         this -> left = NULL;
         this -> right = NULL;
-
     }
 };
 
-node* buildTree(node* root) {
+node* buildTree(node* root)
+{
     cout<<"Enter the data: "<<endl;
-    int data;
-    cin>>data;
-
+    int data; cin>>data;
     root = new node(data);
-    if(data==-1)
-        return NULL;
-    
-    cout<<"Enter data for insertion in left "<<data<<endl;
-    root ->left = buildTree(root->left);
 
-    cout<<"Enter data for insertion in right "<<data<<endl;
+    if(data == -1)
+        return NULL;
+
+    cout<<"Enter data for left of "<<data<<endl;
+    root ->left = buildTree(root->left);
+    cout<<"Enter data for right of"<<data<<endl;
     root->right = buildTree(root->right);
+
+    return root;
 }
 
 void levelOrderTraversal(node* root)
 {
-    queue<node*> q;
+    queue <node*> q;
     q.push(root);
+    q.push(NULL);
 
     while(!q.empty())
     {
         node* temp = q.front();
-        cout<<temp->data<<" ";
         q.pop();
 
-        if(temp ->left)
+        if(temp == NULL)   //purana level compeletely travel ho chukka he
         {
-            q.push(temp->left);
+            cout<<endl;
+            if(!q.empty())  //queue still has some child nodes
+            {
+                q.push(NULL);
+            }
         }
-        if(temp ->right)
+        else
         {
-            q.push(temp->right);
+            cout<<temp->data<<" ";
+            if(temp->left)
+            q.push(temp->left);
+
+            if(temp->right)
+                q.push(temp->right);
         }
     }
+}
+
+void inorder(node* root)   //LNR
+{
+    //base case
+    if(root==NULL)
+        return;
+
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+void preorder(node* root)  //NLR
+{
+    if(root==NULL)
+        return;
+
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(node* root)  //LRN
+{
+    if(root==NULL)
+        return;
+
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
 }
 
 int main()
 {
     node* root = NULL;
 
-    //creating a tree
+    //Creating a tree
     root = buildTree(root);
 
+    //Tree data
     // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
-    //levelOrder
-    cout<<"Printing the level order traversal order "<<endl;
+
+
+    //Level order traversal
+    cout<<"Printing level order traversal "<<endl;
     levelOrderTraversal(root);
+
+    //Inorder traversal
+    cout<<"Inorder traversal is "<<endl;
+    inorder(root);
+    cout<<endl;
+    //Preorder traversal
+    cout<<"Preorder traversal is"<<endl;
+    preorder(root);
+    cout<<endl;
+
+    //Postorder traversal
+    cout<<"Postorder traversal"<<endl;
+    postorder(root);
+    cout<<endl;
 }
